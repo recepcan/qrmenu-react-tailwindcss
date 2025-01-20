57ffb0bf17614d2fc67bc39b37acaf42a6276d2a
Recep CAN <recepcn61@gmail.com>
2025-01-20 16:46:35 +0300

    header padding düzeltmesi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div className='w-full flex items-center justify-center space-x-5 text-xl
+    <div className='w-full flex items-center justify-center space-x-5 text-xl pt-32 
      font-extrabold tracking-wider h-16 sm:h-24 absolute top-0 left-0  '>
    <Link to={'/'} className='p-2 bg-gray-100 shadow-lg shadow-black rounded-lg'>Menu</Link>
     <Link to={'/intheBox'} className='p-2 bg-gray-100 shadow-lg shadow-black rounded-lg'>Sepetim</Link>
