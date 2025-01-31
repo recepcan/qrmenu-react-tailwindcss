6471dad0f8a52068f5f2d36746b82dafe24036e3
Recep CAN <recepcn61@gmail.com>
2025-01-31 21:18:43 +0300

    kategori uı

--- client/src/pages/CurrentUser.jsx
+++ client/src/pages/CurrentUser.jsx
@@ -67,20 +67,21 @@ function CurrentUser() {
     className=' w-full min-h-screen   flex flex-col object-cover py-20 sm:py-32
     items-center   space-y-5'>
     
-<h1 className='p-3 rounded-xl bg-sky-900 text-white text-2xl'>{username}</h1>
+<h1 className='p-3 rounded-xl bg-sky-600 text-white text-2xl'>{username}</h1>
 
     <div className='overflow-auto  w-full grid grid-cols-1 max-[429px]:px-14 min-[429px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 justify-items-center lg:w-[1024px]  gap-5 p-5'>
     {
         userCategory.map((ctg,index)=>(
             <Link to={`products?tab=${ctg.name}`}
              key={index*2} 
-            className='w-full min-h-48 rounded-lg dark:shadow-none shadow-md shadow-gray-400 bg-gray-200 dark:bg-black/70 transition-all duration-300'> 
+            className='w-full min-h-48 rounded-lg dark:shadow-none shadow-md shadow-gray-400 
+            bg-gray-200 dark:bg-gray-700 transition-all duration-300 '> 
             
 
             <div 
                         key={index}
                         className='w-full h-full  bg-cover bg-center p-3 space-y-5
-                        bg-no-repeat rounded-lg border-2 flex flex-col items-center justify-center text-center'
+                        bg-no-repeat rounded-lg  flex flex-col items-center justify-center text-center'
                         
                         >
                         <img src={ctg.image}
@@ -88,7 +89,7 @@ function CurrentUser() {
                         alt="" />
                         <h1 key={index}
                          className=
-                        'p-2  w-full bg-sky-900    rounded-lg text-white font-extrabold text-xl flex items-center justify-center'>
+                        'p-2  w-full bg-sky-600    rounded-lg text-white font-extrabold text-xl flex items-center justify-center'>
                          
                          {ctg.title}
                          
